c762cfbe57c3cb1e007059e17a2ad58906e81ffe
Mehelborn <33414237+Mehelborn@users.noreply.github.com>
2021-05-21 02:52:38 +0700

    DONE!

--- src/Kata.java
+++ src/Kata.java
@@ -1,21 +1,19 @@
 public class Kata {
-  public static String highAndLow(String numbers) {
+  public static String highAndLow(String numbers) {   
     
-    int[] number = new int[12];
-
     String[] splitString = numbers.split(" ");
-
+    int[] number = new int[splitString.length];
     
-    for (int i = 0; i < 12; i++)
+    for (int i = 0; i < number.length; i++)
       number[i] = Integer.parseInt(splitString[i]);
       
     int min = number[0];
     int max = number[0];
       
-    for (int i = 1; i < 12; i++)
+    for (int i = 1; i < number.length; i++)
       if (number[i] < min) min = number[i];
     
-    for (int i = 1; i < 12; i++)
+    for (int i = 1; i < number.length; i++)
       if (number[i] > max) max = number[i];
       
   
