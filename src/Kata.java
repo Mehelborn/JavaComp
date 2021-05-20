b16010ece476c060afbd8fe2a18056db4f9ad693
Mehelborn <33414237+Mehelborn@users.noreply.github.com>
2021-05-21 02:09:26 +0700

    DONE!

--- src/Kata.java
+++ src/Kata.java
@@ -1,48 +1,26 @@
 public class Kata {
-  public static String highAndLow(String numbers) {  
-            
-    int i = 0,
-        num = 0,
-        max = numbers.charAt(i) - '0',
-        min = numbers.charAt(i) - '0',
-        reminder = 0;        
-    boolean isNeg = false;  
-    String thehighest, thelowest;    
-
-    while(i < numbers.length()) {       
-      
-      if (numbers.charAt(i) == '-') {
-    	  isNeg = true;    	  
-          i++;
-          continue;
-      }
-      
-      else if (numbers.charAt(i) == ' ') {
-    	  i++;
-    	  continue;
-      }
-      
-      else if (numbers.charAt(i + 1) == ' ' && reminder == 0) {
-    	  reminder = 0;
-          if(isNeg) num = -num;
-          if(num > max) max = num;
-          if(num < min) min = num;
-          i++;
-          continue;
-      }      
-           
-      else if ((numbers.charAt(i + 1) - '0') >= 1 && (numbers.charAt(i + 1) - '0') <= 9 && numbers.charAt(i + 1) != ' ') {
-          num *= 10;
-          num += numbers.charAt(i) - '0';
-          reminder++;
-      }                  
-        
-      i++;
-    }
-  
-    thehighest = Integer.toString(max);
-    thelowest = Integer.toString(min);
+  public static String highAndLow(String numbers) {
     
-    return thehighest + " " + thelowest;
+    int[] number = new int[12];
+
+    String[] splitString = numbers.split(" ");
+
+    
+    for (int i = 0; i < 12; i++)
+      number[i] = Integer.parseInt(splitString[i]);
+      
+    int min = number[0];
+    int max = number[0];
+      
+    for (int i = 1; i < 12; i++)
+      if (number[i] < min) min = number[i];
+    
+    for (int i = 1; i < 12; i++)
+      if (number[i] > max) max = number[i];
+      
+  
+  String sus = Integer.toString(max) + " " + Integer.toString(min);
+
+    return sus;
   }
 }
